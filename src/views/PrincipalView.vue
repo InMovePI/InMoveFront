1bf08cfdb7c49910677ed8c4d8a579f9c3a86574
isacdcunha <isabellecunhaifc@gmail.com>
2025-10-19 22:42:08 -0300

    feat: inicio da tela dois

--- src/views/PrincipalView.vue
+++ src/views/PrincipalView.vue
@@ -3,7 +3,7 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-   <header>
+  <header>
     <div class="navegaçao">
       <h1>INMOVE</h1>
       <div class="pages">
@@ -14,88 +14,156 @@ import { RouterLink } from 'vue-router'
       </div>
     </div>
   </header>
+
   <main>
-    <div class="texto">
+    <div class="intro">
+      <div class="texto">
         <h2>Olá, usuário!</h2>
         <h1>MAKE YOUR BODY SHAPE</h1>
-        <p>Descubra o prazer de se <span>movimentar</span>,
-        superar desafios e construir uma rotina mais
-        <span>equilibrada</span>. Comece hoje a sua
-        jornada rumo à melhor versão de si mesmo.</p>
+        <p>
+          Descubra o prazer de se <span>movimentar</span>, superar desafios e construir uma rotina
+          mais <span>equilibrada</span>. Comece hoje a sua jornada rumo à melhor versão de si mesmo.
+        </p>
+      </div>
+
+      <img src="/public/mulheres-jovens-participando-da-aula-de-spinning(1).jpg" alt="" />
     </div>
 
-    <img src="/public/mulheres-jovens-participando-da-aula-de-spinning(1).jpg" alt="">
+    <div class="dois">
+      <h1>POR QUÊ ESCOLHER A <span>INMOVE</span>?</h1>
+      <div class="topicos">
+        <div class="container">
+          <h1>PROGRESSO</h1>
+          <p>
+            Acompanhe sua evolução com treinos planejados para desafiar seus limites e gerar
+            resultados reais, passo a passo.
+          </p>
+        </div>
+        <div class="container">
+          <H1>TREINOS PERSONALIZADOS</H1>
+          <p>
+            Cada corpo é único. Por isso, nossos programas são criados com base nos seus objetivos -
+            seja ganhar massa, definir ou melhorar o condicionamento físico.
+          </p>
+        </div>
+      </div>
+      <div class="topicos">
+        <div class="container">
+          <h1>ORIENTAÇÃO NUTRICIONAL</h1>
+          <p>
+            Potencialize seus resultados com orientações nutricionais que complementam o treino e
+            promovem mais energia e bem-estar
+          </p>
+        </div>
+        <div class="container">
+          <h1>AUTODEFESA E CONFIANÇA</h1>
+          <p>
+            Desenvolva força física e mental com atividades que unem preparo corporal e técnicas de
+            defesa pessoal.
+          </p>
+        </div>
+      </div>
+    </div>
   </main>
-
 </template>
 
 <style scoped>
-main {
-  display: flex;
-   justify-content: space-between;
-  align-items: center; 
-  height: 100vh;
-  padding: 0 80px; 
+.container p{
+  color: white;
+  width: 300px;
 }
 
-img{
+.topicos {
+  display: flex;
+  justify-content: space-around;
+  margin: 20px 0px;
+}
+
+.container h1 {
+  color: white;
+  font-size: 20px;
+}
+
+.dois h1 {
+  color: white;
+  margin: 20px 50px;
+}
+
+.dois {
+  background-color: rgba(51, 51, 51, 1);
+}
+
+main {
+  padding: 0 80px;
+  display: flex;
+  flex-direction: column;
+}
+
+.intro {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+}
+
+img {
   width: 800px;
-  height:500px;
+  height: 500px;
   border-radius: 30px;
 }
 
-span{
-    color: rgb(206, 233, 4);
+span {
+  color: rgb(206, 233, 4);
 }
 
-.texto h2{
-    color:rgb(206, 233, 4) ;
-    font-size: 18px;
-    margin: 0px 0px 0px 50px;
-} 
-
-.texto h1{
-    color: white;
-    font-size: 50px;
-    width: 400px;
-    margin: 0px 50px;
-    font-weight: bolder;
+.texto h2 {
+  color: rgb(206, 233, 4);
+  font-size: 18px;
+  margin: 0px 0px 0px 50px;
 }
 
-.texto p{
-    color: white;
-    width: 400px;
-    margin: 0px 50px;
+.texto h1 {
+  color: white;
+  font-size: 50px;
+  width: 400px;
+  margin: 0px 50px;
+  font-weight: bolder;
 }
 
-.cadastro{
-    background-color: rgb(206, 233, 4);
-    padding: 9px;
-    border-radius: 50px;
-    color: black;
-    text-decoration: none
+.texto p {
+  color: white;
+  width: 400px;
+  margin: 0px 50px;
+}
+
+.cadastro {
+  background-color: rgb(206, 233, 4);
+  padding: 9px;
+  border-radius: 50px;
+  color: black;
+  text-decoration: none;
 }
 
 .navegaçao {
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-
   align-items: center;
   background-color: white;
 }
 
-.pages{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 50px;
+.pages {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 50px;
 }
 
-.pages a{
-    color: black;
-    text-decoration: none;
-    position: relative;
+.pages a {
+  color: black;
+  text-decoration: none;
+  position: relative;
 }
 
 .pages a::after {
